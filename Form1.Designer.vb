18da1cb1955922dfbcceece4893d12a527773be1
Sapphire Exile <sohzassassinsteam@gmail.com>
2021-06-29 15:11:21 -0400

    Initial push

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,204 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FlashButton = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.BinaryLabel = New System.Windows.Forms.Label()
+        Me.FlashtoolLabel = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.ComDevLabel = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.FlasherWorker = New System.ComponentModel.BackgroundWorker()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(0, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Download ESPTool"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(588, 13)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 425)
+        Me.Panel1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(200, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Help Panel"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Button2
+        '
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(0, 46)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(200, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Documentation"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'FlashButton
+        '
+        Me.FlashButton.Enabled = False
+        Me.FlashButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.FlashButton.Location = New System.Drawing.Point(13, 415)
+        Me.FlashButton.Name = "FlashButton"
+        Me.FlashButton.Size = New System.Drawing.Size(257, 23)
+        Me.FlashButton.TabIndex = 2
+        Me.FlashButton.Text = "Flash bin"
+        Me.FlashButton.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Location = New System.Drawing.Point(13, 13)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Binary"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'BinaryLabel
+        '
+        Me.BinaryLabel.Location = New System.Drawing.Point(94, 13)
+        Me.BinaryLabel.Name = "BinaryLabel"
+        Me.BinaryLabel.Size = New System.Drawing.Size(488, 23)
+        Me.BinaryLabel.TabIndex = 4
+        Me.BinaryLabel.Text = "No directory set"
+        Me.BinaryLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'FlashtoolLabel
+        '
+        Me.FlashtoolLabel.Location = New System.Drawing.Point(94, 46)
+        Me.FlashtoolLabel.Name = "FlashtoolLabel"
+        Me.FlashtoolLabel.Size = New System.Drawing.Size(488, 23)
+        Me.FlashtoolLabel.TabIndex = 6
+        Me.FlashtoolLabel.Text = "No directory set"
+        Me.FlashtoolLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Button5
+        '
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Location = New System.Drawing.Point(13, 46)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Flashtool"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.FileName = "OpenFileDialog2"
+        '
+        'ComDevLabel
+        '
+        Me.ComDevLabel.Location = New System.Drawing.Point(93, 75)
+        Me.ComDevLabel.Name = "ComDevLabel"
+        Me.ComDevLabel.Size = New System.Drawing.Size(488, 23)
+        Me.ComDevLabel.TabIndex = 8
+        Me.ComDevLabel.Text = "No directory set"
+        Me.ComDevLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Location = New System.Drawing.Point(12, 75)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Get COM"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'FlasherWorker
+        '
+        Me.FlasherWorker.WorkerReportsProgress = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(277, 415)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(304, 23)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(9, 146)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(573, 266)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "FLASHING DO NOT EXIT APPLICATION"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.ComDevLabel)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.FlashtoolLabel)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.BinaryLabel)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.FlashButton)
+        Me.Controls.Add(Me.Panel1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.Name = "Form1"
+        Me.Text = "Sapphire's ESP Flasher"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FlashButton As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents BinaryLabel As Label
+    Friend WithEvents FlashtoolLabel As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+    Friend WithEvents ComDevLabel As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents FlasherWorker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label2 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,167 @@
-﻿Public Class Form1
+﻿Imports System.Text.RegularExpressions
+Imports System.Management
 
+Public Class Form1
+
+    Dim FlashToolLocationVerified = False
+    Dim BinaryLocationVerified = False
+    Dim COMVerified = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FlashtoolLabel.Text = My.Settings.LastFlashLocation
+        BinaryLabel.Text = My.Settings.LastBinLocation
+
+        If (My.Computer.FileSystem.FileExists(Application.StartupPath + "\esptool.exe")) Then
+            If (My.Settings.HasSeenFlashDirectoryWarning = False) Then
+                MsgBox("Application is running in the same path as esptool, will use this tool over the selected one. To avoid this in the future, place this tool elsewhere. THIS MESSAGE WILL ONLY BE SHOWN ONCE.")
+                My.Settings.HasSeenFlashDirectoryWarning = True
+            End If
+
+            My.Settings.LastFlashLocation = Application.StartupPath + "\esptool.exe"
+            FlashtoolLabel.Text = Application.StartupPath + "\esptool.exe"
+            FlashToolLocationVerified = True
+
+            My.Settings.Save()
+        End If
+
+        If (My.Computer.FileSystem.FileExists(Application.StartupPath + "\ESP.bin")) Then
+            If (My.Settings.HasSeenDefaultBinWarning = False) Then
+                MsgBox("Default override ESP.bin found in this path, will default to this binary unless another is selected. THIS MESSAGE WILL ONLY BE SHOWN ONCE.")
+                My.Settings.HasSeenDefaultBinWarning = True
+            End If
+
+            My.Settings.LastBinLocation = Application.StartupPath + "\ESP.bin"
+            BinaryLabel.Text = Application.StartupPath + "\ESP.bin"
+            BinaryLocationVerified = True
+            My.Settings.Save()
+        End If
+
+        Dim ComX = findComDevice("Silicon Labs CP210x USB to UART Bridge")
+        If (ComX.StartsWith("COM")) Then
+            ComDevLabel.Text = ComX
+            COMVerified = True
+        End If
+
+        EnableButtonX()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Process.Start("https://github.com/espressif/esptool/releases/")
+    End Sub
+
+    Private Sub EnableButtonX()
+        If (FlashToolLocationVerified = True And BinaryLocationVerified = True And COMVerified = True) Then
+            FlashButton.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        OpenFileDialog1.ShowDialog()
+        Dim FilenameX = OpenFileDialog1.FileName
+
+        If (FilenameX.EndsWith(".bin")) Then
+            BinaryLabel.Text = FilenameX
+            BinaryLocationVerified = True
+            My.Settings.LastBinLocation = FilenameX
+            My.Settings.Save()
+        Else
+            BinaryLabel.Text = "File did not have the .bin extension, retry."
+        End If
+
+        EnableButtonX()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        OpenFileDialog1.ShowDialog()
+        Dim FileX = OpenFileDialog1.FileName
+
+        If (FileX.Contains("esptool.exe")) Then
+            FlashtoolLabel.Text = FileX
+            FlashToolLocationVerified = True
+            My.Settings.LastFlashLocation = FileX
+            My.Settings.Save()
+        Else
+            FlashtoolLabel.Text = "Invalid, or renamed tool. Retry."
+        End If
+
+        EnableButtonX()
+    End Sub
+
+    Private Sub FlashButton_Click(sender As Object, e As EventArgs) Handles FlashButton.Click
+        FlasherWorker.RunWorkerAsync()
+    End Sub
+
+    Public Function findComDevice(ByVal PartialName As String) As String
+        Try
+            Dim moReturn As Management.ManagementObjectCollection
+            Dim moSearch As Management.ManagementObjectSearcher
+            Dim mo As Management.ManagementObject
+            moSearch = New Management.ManagementObjectSearcher("Select * from Win32_PnPEntity")
+            moReturn = moSearch.Get
+
+            For Each mo In moReturn
+                If IsNothing(mo.Properties.Item("Name").Value) Then Continue For
+                If CStr(mo.Properties.Item("Name").Value).ToUpper.Contains(PartialName.ToUpper) Then
+                    'returns something like: "Prolific USB-to-Serial Comm Port (COM17)"
+                    Dim IPRegEx As Regex = New Regex("(?<=\().*?(?=\))")
+                    Return IPRegEx.Match(mo.Properties.Item("Name").Value.ToString).Value
+
+                End If
+            Next
+            Return Nothing
+        Catch ex As Exception
+            Console.WriteLine(ex.ToString)
+            Return Nothing
+        End Try
+
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim ComX = findComDevice("Silicon Labs CP210x USB to UART Bridge")
+        If (ComX.StartsWith("COM")) Then
+            ComDevLabel.Text = ComX
+            COMVerified = True
+        End If
+    End Sub
+
+    Private Sub FlasherWorker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles FlasherWorker.DoWork
+
+        Dim Argbuilder As String = "--port " + ComDevLabel.Text + " --baud 921600 write_flash -fm dio 0x00000 " + BinaryLabel.Text
+
+        Dim oProcess As New Process()
+        Dim oStartInfo As New ProcessStartInfo(FlashtoolLabel.Text, Argbuilder)
+        oStartInfo.UseShellExecute = False
+        oStartInfo.RedirectStandardOutput = True
+        oStartInfo.RedirectStandardError = True
+        oProcess.StartInfo = oStartInfo
+        oProcess.Start()
+
+        Dim sOutput As String
+        Using oStreamReader As System.IO.StreamReader = oProcess.StandardOutput
+            sOutput = oStreamReader.ReadToEnd()
+
+            Console.WriteLine(sOutput)
+
+            Dim Finderstart
+            Dim FinderEnd
+            Dim PercentLabel
+            If (sOutput.Contains("%")) Then
+                Try
+                    Finderstart = sOutput.IndexOf("(")
+                    FinderEnd = sOutput.IndexOf(")")
+                    PercentLabel = sOutput.Substring(sOutput.IndexOf("("), sOutput.Length() - sOutput.IndexOf(")") - 2)
+                    FlasherWorker.ReportProgress(PercentLabel)
+                Catch ex As Exception
+
+                End Try
+
+            End If
+        End Using
+        Console.WriteLine(sOutput)
+    End Sub
+
+    Private Sub FlasherWorker_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles FlasherWorker.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
 End Class
